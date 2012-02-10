4e94b21552297ac1c3288a8d88e1655cfac7ef27
Hideki Hashizume <hideki.hashizume@gmail.com>
2012-02-11 00:17:48 +0900

    Create

---
+++ MoqSample.Test/ProductLogicTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MoqSample.Test
+{
+    [TestClass]
+    public class ProductLogicTest
+    {
+        [TestMethod]
+        public void CalculateTaxTest()
+        {
+            //Arrange
+            var dao = new Mock<IProductDao>();
+            dao.Setup(m => m.GetItemById(1))
+                .Returns(new Product
+                {
+                    Id = 1,
+                    Name = "Book",
+                    Price = 300
+                });
+            var target = new ProductLogic { ProductDao = dao.Object };
+
+            //Act
+            var result = target.CalculateTax(1);
+
+            //Assert
+            Assert.AreEqual(15, result);
+
+        }
+
+        [TestMethod]
+        public void EditProductTest()
+        {
+            //Arrange
+            //DBアクセス部分をモックで作成
+            var dao = new Mock<IProductDao>();
+            dao.Setup(m => m.GetItemById(10))
+                .Returns(new Product
+                                {
+                                    Id = 10,
+                                    Name = "Book",
+                                    Price = 300
+                                });
+            dao.Setup(m => m.Insert(It.IsAny<Product>()))
+                .Callback<Product>(p =>
+                                        {
+                                            //コールバックにてAssertを実行
+                                            Assert.AreEqual(10, p.Id);
+                                            Assert.AreEqual("Book", p.Name);
+                                            Assert.AreEqual(315, p.Price);
+                                        });
+            var target = new ProductLogic { ProductDao = dao.Object };
+
+            //Act
+            target.EditProduct(new Product { Id = 10 });
+
+            //Assert(一部)
+            dao.Verify(m => m.Insert(It.IsAny<Product>()), Times.Once());
+            dao.Verify(m => m.Update(It.IsAny<Product>()), Times.Never());
+        }
+
+    }
+}
---
+++ MoqSample/ProductLogic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace MoqSample
+{
+    /// <summary>
+    /// テスト対象のクラス
+    /// </summary>
+    public class ProductLogic
+    {
+        public IProductDao ProductDao { get; set; }
+
+        public decimal CalculateTax(int id)
+        {
+            var domain = ProductDao.GetItemById(id);
+            var result = domain.Price * 0.05m;
+            return result;
+        }
+
+        public void EditProduct(Product item)
+        {
+            var id = item.Id;
+            var domain = ProductDao.GetItemById(id);
+            domain.Price *= 1.05m;
+            ProductDao.Insert(domain);
+        }
+    }
+
+    /// <summary>
+    /// モックで置き換える予定のDaoのインターフェイス
+    /// </summary>
+    public interface IProductDao
+    {
+        void Update(Product product);
+        void Insert(Product product);
+        Product GetItemById(int id);
+        List<Product> GetAllItems();
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
